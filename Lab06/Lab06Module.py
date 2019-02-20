852d1d2fc4ee549eeb418fbb55c8a31c6b7252cd
ee364g02 <mohdshah@purdue.edu>
2019-02-20 17:14:18 -0500

    done

---
+++ Lab06/Lab06Module.py
@@ -0,0 +1,31 @@
+#######################################################
+#    Author: Nur Nadhira Aqilah Binti Mohd Shah
+#    email: mohdshah@purdue.edu
+#    ID: mohdshah
+#    Date: 2/13
+#######################################################
+import os      # List of  module  import  statements
+import sys     # Each  one on a line
+import re
+# Module  level  Variables. (Write  this  statement  verbatim .)
+#######################################################
+
+def extractArguments(commandline: str)-> list:
+    #scriptBash switch(switch name) (value)
+    temp = re.findall(r'[\\+]([a-z]){1}\s([^\\+][\S]*)', commandline)
+    pattern = sorted(temp,key=str)
+
+    return pattern
+
+def extractNumerics(sentence:str)-> list:
+    #integers: start with - or +
+    #regular floats: start with - or +, have a decimal point
+    #scientific notation: start with - or +, single digit before decimal
+    pattern = re.findall(r'([-+]?[^[a-zA-Z][\d\.][eE\-\+\d]+|[-+]?\d+\.?\d)', sentence)
+
+    return pattern
+
+# This  block  is  optional
+if __name__  == "__main__":
+# Write  anything  here to test  your  code.
+   ...
